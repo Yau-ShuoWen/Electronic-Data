2332bec6f8cc1671951a2e0239a456f83b4fac9a
steamed twisted rolls <yausw13@gmail.com>
2026-01-23 23:09:27 +0800

    1/21

--- src/Tool/Code/test.java
+++ src/Tool/Code/test.java
@@ -1,9 +1,18 @@
 package Tool.Code;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class test
 {
     public static void main(String[] args)
     {
+
+        System.out.println((Arrays.toString("0123456789".split(""))));
+
+       
     }
 }
 
