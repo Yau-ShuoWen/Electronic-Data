0f14f2cda89a26c9cf5ab73df8f9cc9913aa1326
steamed twisted rolls <yausw13@gmail.com>
2026-04-15 22:51:05 +0800

    -

--- src/Tool/Code/test.java
+++ src/Tool/Code/test.java
@@ -4,14 +4,17 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.*;
+import java.util.Objects;
 
 public class test
 {
     public static void main(String[] args)
     {
-
-        System.out.println((Arrays.toString("0123456789".split(""))));
-
+        List<Object> list = new ArrayList<>();
+        list.add(1);
+        list.add(list);
+        System.out.println(list);
     }
 }
 
