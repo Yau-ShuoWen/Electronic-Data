07c0236d7fd12fecfe64509b35c373ab04d2a04d
steamed twisted rolls <yausw13@gmail.com>
2026-05-04 21:34:34 +0800

    间歇性越南语收集改造计划精神病

--- src/Tool/Code/test.java
+++ src/Tool/Code/test.java
@@ -1,12 +1,34 @@
 package Tool.Code;
 
 
+import java.util.Scanner;
 
 public class test
 {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
 
+        String s = sc.nextLine();
+
+        System.out.println(s
+                .replace("ph", "f")
+                .replace("th", "ŧ")
+                .replace("kh", "ħ")
+                .replace("ng", "ŋ")
+                .replace("ngh", "ŋ")
+                .replace("nh", "ñ")
+                .replace("gi", "ż")
+                .replace("g", "ǥ")
+                .replace("gh", "ǥ")
+                .replace("tr", "ĉ")
+                .replace("ch", "ċ")
+                .replace("uy", "ÿ")
+
+
+        );
+
+        main(args);
     }
 }
 
